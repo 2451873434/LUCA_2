4d001217d30939e5727cf85dc4bcc3faa0d4ab07
2451873434 <2451873434@qq.com>
2021-02-19 22:13:48 +0800

    pm

--- find_ball.py
+++ find_ball.py
@@ -10,14 +10,14 @@ contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_S
 i=-1;
 for c in contours:
     i=i+1
-    # if hierarchy[0][i][2]!=-1:
-    # 计算最小封闭圆形的中心和半径
-    (x, y), radius = cv2.minEnclosingCircle(c)
-    # 转换成整数
-    center = (int(x), int(y))
-    radius = int(radius)
-    # 画出圆形
-    img = cv2.circle(img, center, radius, (0, 255, 0), 1)
+    if hierarchy[0][i][2]!=-1:
+        # 计算最小封闭圆形的中心和半径
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        # 转换成整数
+        center = (int(x), int(y))
+        radius = int(radius)
+        # 画出圆形
+        img = cv2.circle(img, center, radius, (0, 255, 0), 1)
 cv2.drawContours(img, contours, -1, (0, 0, 0), 1)
 cv2.imshow("contours", img)
 cv2.waitKey()
